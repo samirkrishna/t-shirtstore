119e365fb56871ba52d11bb50aee361a310ce5ed
Samir Krishna Cholleti <samirkrishna.cholleti@infor.com>
2022-02-07 21:03:59 +0530

    Added user modal

---
+++ models/user.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    maxlength: [40, "Please provide name lessthan 40 chars"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    validate: [validator.isEmail, "Please enter email in correct format"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: [6, "password should be atleast 6 char"],
+    select: false,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  photo: {
+    id: {
+      type: String,
+      required: true,
+    },
+    secure_url: {
+      type: String,
+      required: true,
+    },
+  },
+  forgotPasswordToken: String,
+  forgotPasswordExpiry: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Encrypt password before save -HOOKS
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+//Validate the password with user passed password
+userSchema.methods.isValidPassword = async function (userSentPass) {
+  return await bcrypt.compare(userSentPass, this.password);
+};
+
+//create and return a Jwt token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
+};
+
+module.exports = mongoose.model("User", userSchema);
